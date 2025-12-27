70ba7b2b5604f082eaab05ddc83984a777f20185
Akanksha Kapratwar <kapratwarakanksha@gmail.com>
2025-12-27 21:52:53 +0530

    docs

---
+++ abn1.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ABNegativeBag1 extends StatefulWidget {
+  const ABNegativeBag1({super.key});
+
+  @override
+  State<ABNegativeBag1> createState() => _ABNegativeBag1State();
+}
+
+class _ABNegativeBag1State extends State<ABNegativeBag1> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          "Information",
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+                child: Image.asset("assets/Jankalyan.jpg",
+                    height: 250, width: 250)),
+            SizedBox(height: 20),
+            Text(
+              "Blood Bank Name : Janakalyan Blood Bank",
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+            Text(
+              "Available Blood Group Name : AB-",
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+            Text(
+              "Location : Saras Baug Rd, Dadawadi, Shukrawar Peth, Pune, Maharashtra (411002)",
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+            Text(
+              "Contact Number : 020 2444 9527 ",
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
